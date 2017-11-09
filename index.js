c1f92b661c9c973b074ad14478c22ecfe1923fae
hellomrbigshot <1994yechen@gmail.com>
2017-11-10 00:08:00 +0800

    add signup page

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ app.use(session({
 // flash 中间件, 用来显示
 app.use(flash());
 
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+	uploadDir: path.join(__dirname, 'public/img'), // 上传文件目录
+	keepExtensions: true // 保留后缀
+}))
+
 // 设置模板全局常量
 app.locals.blog = {
 	title: pkg.name,
@@ -41,7 +47,7 @@ app.locals.blog = {
 }
 
 // 添加模板必需的三个变量
-app.use( (req, res, next) = {
+app.use( (req, res, next) => {
 	res.locals.user = req.session.user;
 	res.locals.success = req.flash('success').toString();
 	res.locals.error = req.flash('error').toString();
