ca1cf880d5a44e5a35b2a715c7b0346b90ed6489
hellomrbigshot <1994yechen@gmail.com>
2017-11-13 23:36:50 +0800

    add post page

---
+++ models/posts.js
@@ -0,0 +1,55 @@
+const Post = require('../lib/mongo').Post
+const marked = require('marked')
+
+
+// 将 post 的 content 从 markdown 转成 html
+Post.plugin('contentToHtml', {
+	afterFind (posts) {
+		return posts.map(post => {
+			post.content = marked(post.content)
+			return post
+		})
+	},
+	afterFindOne (post) {
+		if (post) {
+			post.content = marked(post.content)
+		}
+		return post
+	}
+})
+module.exports = {
+	// 创建一篇文章
+	create (post) {
+		return Post.create(post).exec()
+	},
+	getPostById (postId) {
+		return Post
+			.findOne({ _id: postId })
+			.populate({ path: 'author', model: 'User' })
+			.addCreatedAt()
+			.contentToHtml()
+			.exec()
+	},
+
+	// 按照创建时间降序获取所有用户文章或者某个特定用户的所有文章
+	getPosts (author) {
+		const query = {}
+		if (author) {
+			query.author = author
+		}
+		return Post
+			.find(query)
+			.populate({ path: 'author', model: 'User'})
+			.sort({ _id: -1})
+			.addCreatedAt()
+			.contentToHtml()
+			.exec()
+	},
+
+	// 通过文章 id 给 pv 加 1
+	incPv (postId) {
+		return Post
+			.update({ _id: postId }, { $inc: { pv: 1 } })
+			.exec()
+	}
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const PostModel = require('../models/posts')
 
 const checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /posts 所有用户或者特定用户的文章页
 // eg: GET /posts?author=xxx
 router.get('/', (req, res, next) => {
-	// res.send(req.flash());
-	res.render('posts')
+	const author = req.query.author
+
+	PostModel.getPosts(author)
+		.then(posts => {
+			res.render('posts', {
+				posts
+			})
+		})
+		.catch(next)
 })
 
-// POST /posts 发表一篇文章
-router.post('/', checkLogin, (req, res, next) => {
-	res.send(req.flash());
+// POST /posts/create 发表一篇文章
+router.post('/create', checkLogin, (req, res, next) => {
+	const author = req.session.user._id
+	const title = req.fields.title
+	const content = req.fields.content
+
+	// 校验参数
+	try {
+		if (!title.length) {
+			throw new Error('请填写标题')
+		}
+		if (!content.length) {
+			throw new Error('请填写内容')
+		}
+	} catch (e) {
+		req.flash('error', e.message)
+		return res.redirect('back')
+	}
+
+	let post = {
+		author,
+		title,
+		content,
+		pv: 0
+	}
+
+	PostModel.create(post)
+		.then((result) => {
+			// 此 post 是插入 mongodb 后的值， 包含 _id
+			post = result.ops[0]
+			req.flash('success', '发表成功')
+			// 发表成功后跳转到该文章页
+			res.redirect(`/posts/${post._id}`)
+		})
+		.catch(next)
 })
 
 // GET /posts/create 发表文章页
 router.get('/create', checkLogin, (req, res, next) => {
-	res.send(req.flash());
+	res.render('create')
 })
 
 // GET /posts/:postId 单独一篇的文章页
