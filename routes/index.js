23bfd6506e44005c6ae216f05bfe2b7d7bf792e7
hellomrbigshot <1994yechen@gmail.com>
2017-11-24 23:23:04 +0800

    add 404

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 module.exports = function (app) {
 	app.get('/', (req, res) => {
-		res.redirect('/posts');
+		res.redirect('/posts')
 	})
-	app.use('/signup', require('./signup'));
-	app.use('/signin', require('./signin'));
-	app.use('/signout', require('./signout'));
-	app.use('/posts', require('./posts'));
+	app.use('/signup', require('./signup'))
+	app.use('/signin', require('./signin'))
+	app.use('/signout', require('./signout'))
+	app.use('/posts', require('./posts'))
+
+	// 404 page
+	app.use((req, res) => {
+		if (!res.headersSent) {
+			res.status(404).render('404')
+		}
+	})
 }
